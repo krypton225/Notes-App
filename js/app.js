0727ec50dd7d494831585e49f0bee226f21fa79e
krypton225 <k.mohamed.11.98@gmail.com>
2022-06-21 10:38:11 +0200

    EDIT funcs of header modal

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-const modalHeaderOpen = () => {
+const modalHeader = () => {
+  // * Open the modal when click on the button in the header.
   document.getElementById("addNewNoteBtn").addEventListener("click", (e) => {
     e.preventDefault();
     document.getElementById("headerModal").classList.add("header-modal-open");
@@ -7,16 +8,23 @@ const modalHeaderOpen = () => {
     myDiv.className = "layer";
 
     document.body.appendChild(myDiv);
+
+    // * Close the modal when click on the close button in the modal body.
+    const closeOfHeaderModal = () => {
+      let closeHeaderModal = document.getElementById("closeHeaderModal");
+      closeHeaderModal.addEventListener("click", () => {
+        document
+          .getElementById("headerModal")
+          .classList.remove("header-modal-open");
+
+        setTimeout(() => {
+          myDiv.classList.remove("layer");
+        }, 600);
+      });
+    };
+
+    closeOfHeaderModal();
   });
 };
 
-modalHeaderOpen();
-
-const closeHeaderModal = () => {
-  document.getElementById("closeHeaderModal").addEventListener("click", (e) => {
-    e.preventDefault();
-    document
-      .getElementById("closeHeaderModal")
-      .classList.remove("header-modal-open");
-  });
-};
+modalHeader();
